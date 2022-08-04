675fece2c545fe99e485f40d4983f3610d703388
jainen1 <jainen@kaufmannfamily.com>
2022-08-04 13:58:04 -0700

    Added three items

---
+++ src/main/java/net/trevorskullcrafter/trevorssentinels/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.trevorskullcrafter.trevorssentinels.item;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.trevorskullcrafter.trevorssentinels.trevorssentinels;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import static net.trevorskullcrafter.trevorssentinels.trevorssentinels.MOD_ID;
+
+public class ModItems {
+    public static final Item SENTINUM_SHARD = registerItem("sentinum_shard",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item BLOOD_DIAMOND = registerItem("blood_diamond",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item VIOLENT_CATALYST = registerItem("violent_catalyst",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registry.ITEM, new Identifier(trevorssentinels.MOD_ID, name), item);
+    }
+    public static void registerModItems(){
+        trevorssentinels.LOGGER.info("Registering Mod Items for "+ MOD_ID);
+    }
+}
